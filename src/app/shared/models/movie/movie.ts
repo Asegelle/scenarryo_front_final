de4101f8848762d85b82ac2fc784ae4059802f21
DESKTOP-5L7N2A8\ib <alexandre.segelle@hotmail.fr>
2021-05-26 15:31:05 +0200

    update display movies from table movie

--- src/app/shared/models/movie/movie.ts
+++ src/app/shared/models/movie/movie.ts
@@ -1,14 +1,34 @@
 export class Movie {
-            constructor(
-                public id?:number,
-                public title?:string,
-                public releaseDate?:Date,
-                public ageLimited?:string,
-                public synopsis?:string,
-                public duration?:number,
-                public poster?:string
-            ){
+            
 
+    id?:number;
+    title?:string;
+    producer?:string;
+    releaseDate?:Date;
+    ageLimited?:string;
+    synopsis?:string;
+    duration?:string;
+    poster?:string;
+    //movieShow?:Show[];
+
+    constructor(id?: number, title?:string, producer?:string,releaseDate?:Date,ageLimited?:string, synopsis?:string, duration?:string, poster?:string){
+        this.id=id;
+        this.title=title;
+        this.producer=producer;
+        this.releaseDate=releaseDate;
+        this.ageLimited=ageLimited;
+        this.synopsis=synopsis;
+        this.duration=duration;
+        this.poster=poster;
+        //this.movieShow=movieShow;
+
+    }
+
+
+        /*
+            fullDescription():string {
+                return this.id+' '+this.title+' '+this.producer+' '+this.releaseDate+' '+this.ageLimit+' '+this.synopsis+' '+this.duration;
             }
+        */
 
 }
