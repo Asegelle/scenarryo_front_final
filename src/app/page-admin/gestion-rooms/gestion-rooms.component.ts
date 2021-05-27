386d125d9fabcb49742e0a622a21c1f0361df6d9
Yousra <yousra.shaki95@gmail.com>
2021-05-27 16:25:25 +0200

    -- addRoom, deleteRoom functionalities

--- src/app/page-admin/gestion-rooms/gestion-rooms.component.ts
+++ src/app/page-admin/gestion-rooms/gestion-rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Room } from 'src/app/shared/models/room';
 import { RoomWebService } from 'src/app/shared/webservices/room/room.webservice';
 
@@ -13,9 +14,38 @@ export class GestionRoomsComponent implements OnInit {
   constructor(private roomWebService : RoomWebService) { }
 
   ngOnInit(): void {
+    this.reloadDataRooms();
+  }
+
+  handleClickDeleteRoom(index : number):void{
+    // TODO  si une room est dans show-room ne supprime pas ca 
+    console.log('deleted ');
+    this.roomWebService.deleteRoom(index)
+    .subscribe(data => {
+      console.log(data);
+      this.reloadDataRooms();
+    },
+    error => {
+      console.log(error);
+      alert('Cette salle contient des SÉANCE(S) planifiée(s), impossible de la supprimée');
+    });
+  }
+  handleClickAddRoom(formAddRoom : NgForm){
+    let room : Room = formAddRoom.value;
+    formAddRoom.reset();
+    this.roomWebService.addRoom(room)
+    .subscribe(data => {
+      this.reloadDataRooms();
+    },
+    error => {
+      console.log(error);
+      alert('Erreur lors de l\'ajout de la salle, ressayez ');
+    });
+  }
+
+  reloadDataRooms(){
     this.roomWebService.getRooms()
     .subscribe( data => {
-      console.log('data');
       this.roomList = data;
     })
   }
--- src/app/shared/webservices/room/room.webservice.ts
+++ src/app/shared/webservices/room/room.webservice.ts
@@ -7,15 +7,31 @@ import { Room } from "../../models/room";
   providedIn: 'root'
 })
 export class RoomWebService{
-
+  
+  
   private urlRoom: string;
   // room : Room;
-
+  
   constructor(private http : HttpClient){
-    this.urlRoom = 'http://localhost:8080/admin/room';
+    this.urlRoom = 'http://localhost:8080/admin/room/';
   }
   getRooms():Observable<Room[]>{
-
-    return this.http.get<Room[]>(this.urlRoom);
+    
+    return this.http.get<Room[]>(this.urlRoom+'getAll');
   }
+  
+  deleteRoom(index: number):Observable<Room> {
+    return this.http.delete<Room>(this.urlRoom+'delete/'+index);
+  }
+  
+  addRoom(room: Room):Observable<Room> {
+    return this.http.post(this.urlRoom+'add',room);
+  }
+  // deleteSchedules(id:number): Observable<Schedule>{
+    
+    //   console.log(this.http.delete<Schedule>(${this.deleteScheduleUrl}/${id}));
+    
+    //   return this.http.delete<Schedule>(${this.deleteScheduleUrl}/${id});
+  // }
+  
 }
