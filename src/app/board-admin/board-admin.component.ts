5b68c75769528b44a8fddfd25215d04d5cb06f49
DESKTOP-5L7N2A8\ib <alexandre.segelle@hotmail.fr>
2021-06-06 08:54:35 +0200

    last changes css

--- src/app/board-admin/board-admin.component.ts
+++ src/app/board-admin/board-admin.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
+import { Room } from '../shared/models/room';
 import { UserService } from '../shared/webservices/login-signup/user.service';
+import { RoomWebService } from '../shared/webservices/room/room.webservice';
 
 @Component({
   selector: 'app-board-admin',
@@ -8,8 +12,24 @@ import { UserService } from '../shared/webservices/login-signup/user.service';
 })
 export class BoardAdminComponent implements OnInit {
   content?: string;
+  roomList:Room[] =[];
+  constructor(private roomWebService : RoomWebService,private userService: UserService) { }
 
-  constructor(private userService: UserService) { }
+  
+  barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  barChartLabels: Label[] = ['salle 0', 'salle 2', 'salle 3', 'salle 4', 'salle 5'];
+  barChartType: ChartType = 'bar';
+  barChartLegend = true;
+  barChartPlugins = [];
+
+  barChartData: ChartDataSets[] = [
+    { data: [7, 0, 0, 0, 0], label: '	Nombre de séances programmées pas salle' }
+  ];
+
+
+
 
   ngOnInit(): void {
     this.userService.getAdminBoard().subscribe(
@@ -20,5 +40,19 @@ export class BoardAdminComponent implements OnInit {
         this.content = JSON.parse(err.error).message;
       }
     );
+
+    this.reloadDataRooms();
   }
+
+
+  reloadDataRooms(){
+    this.roomWebService.getRooms()
+    .subscribe( data => {
+      this.roomList = data;
+     
+      
+    })
+  }
+
+
 }
